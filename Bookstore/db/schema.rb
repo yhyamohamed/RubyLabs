0af448945a9c24e1e360f05b0c6c89add7763e8f
yhyamohamed <yhyashehata1@gmail.com>
2022-06-24 13:37:47 +0200

    adding seeders for users & products

--- Bookstore/db/schema.rb
+++ Bookstore/db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_24_101415) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "pass"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- Bookstore/db/seeds.rb
+++ Bookstore/db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#
+require 'faker'
+Product.destroy_all
+20.times do |i|
+  Product.create(
+    name: Faker::Food.dish,
+    description: Faker::Lorem.sentence(word_count: 10, supplemental: false, random_words_to_add: 0).chop,
+    price: 60.03,
+    image: "#{i + 1}.jpg"
+  )
+end
+
+User.destroy_all
+User.create(name: 'yaya ', email:'yaya @iti.com',password:'yaya',password_confirmation:'yaya')
+
+5.times do |i|
+  f_name = Faker::Name.first_name
+  User.create(
+        name: f_name,
+         email: Faker::Internet.email(name: f_name),
+        password:"yaya_#{i}",
+        password_confirmation:"yaya_#{i}"
+  )
+end
+
